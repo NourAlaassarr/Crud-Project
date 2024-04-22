ad4da02c6d115805669d58bcc10f5d8cd087f22f
Nour Ayman <104293377+NourAlaassarr@users.noreply.github.com>
2024-04-22 02:42:56 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,54 @@
+
+var Name=document.getElementById("ProductNameInput");
+var Price=document.getElementById("ProductPriceInput");
+var Category=document.getElementById("ProductCategoryInput");
+var Description=document.getElementById("ProductDescriptionInput");
+
+var ProductsArray
+
+if(localStorage.getItem!=null){
+    ProductsArray=JSON.parse(localStorage.getItem("Products"));
+    displayProducts(ProductsArray);
+}
+else{
+    ProductsArray=[]
+}
+var Cartoona=``
+function AddProduct(){
+
+    var Product={
+        Name:Name.value,
+        Price:Price.value,
+        Category:Category.value,
+        Description:Description.value
+    };
+    ProductsArray.push(Product);
+     localStorage.setItem("Products",JSON.stringify(ProductsArray));
+    console.log(ProductsArray);
+ClearForm();
+displayProducts(ProductsArray);
+}
+function ClearForm(){
+
+    Name.value=""
+    Price.value=""
+    Category.value=""
+    Description.value=""
+};
+function displayProducts(ProductList){
+    var Cartoona=``;
+    for(i=0;i<ProductList.length;i++){
+        Cartoona+=` <tr>
+        <td>${i}</td>
+        <td>${ProductList[i].Name}</td>
+        <td>${ProductList[i].Price}</td>
+        <td>${ProductList[i].Category}</td>
+        <td>${ProductList[i].Description}</td>
+        <td><button class="btn btn-outline-success">Update</button></td>
+        <td><button class="btn btn-outline-danger">Delete</button></td>
+    </tr>`
+
+    };
+    document.getElementById("tbody").innerHTML=Cartoona;
+
+}
