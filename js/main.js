587f98a8897b1494ac048cd44d8ef47437760d3a
Nour Ayman <104293377+NourAlaassarr@users.noreply.github.com>
2024-04-25 19:28:58 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 
-var Name=document.getElementById("ProductNameInput");
-var Price=document.getElementById("ProductPriceInput");
-var Category=document.getElementById("ProductCategoryInput");
-var Description=document.getElementById("ProductDescriptionInput");
-
+var ProductNameInput=document.getElementById("ProductNameInput");
+var ProductPriceInput=document.getElementById("ProductPriceInput");
+var ProductCategoryInput=document.getElementById("ProductCategoryInput");
+var ProductDescriptionInput=document.getElementById("ProductDescriptionInput");
+var Addbtn=document.getElementById("addBtn");
+var UpdateButtton=document.getElementById("updateBtn");
 var ProductsArray;
 
 if(localStorage.getItem("Products")!=null){
@@ -16,25 +17,34 @@ else{
 var Cartoona=``
 function AddProduct(){
 
-    var Product={
-        Name:Name.value,
-        Price:Price.value,
-        Category:Category.value,
-        Description:Description.value
-    };
-    ProductsArray.push(Product);
-     localStorage.setItem("Products",JSON.stringify(ProductsArray));
-    console.log(ProductsArray);
-ClearForm();
-displayProducts(ProductsArray);
+    if(validation()==true){
+        var Product={
+            Name:ProductNameInput.value,
+            Price:ProductPriceInput.value,
+            Category:ProductCategoryInput.value,
+            Description:ProductDescriptionInput.value
+        };
+        ProductsArray.push(Product);
+         localStorage.setItem("Products",JSON.stringify(ProductsArray));
+        console.log(ProductsArray);
+    ClearForm();
+    displayProducts(ProductsArray);
+    
+    }
+    else{
+        alert("invalid");
+    }
+    
 }
 function ClearForm(){
-
-    Name.value=""
-    Price.value=""
-    Category.value=""
-    Description.value=""
+    
+    ProductNameInput.value=""
+    ProductPriceInput.value=""
+    ProductCategoryInput.value=""
+    ProductDescriptionInput.value=""
 };
+
+
 function displayProducts(ProductList){
     var Cartoona=``;
     for(i=0;i<ProductList.length;i++){
@@ -44,16 +54,13 @@ function displayProducts(ProductList){
         <td>${ProductList[i].Price}</td>
         <td>${ProductList[i].Category}</td>
         <td>${ProductList[i].Description}</td>
-        <td><button class="btn btn-outline-success">Update</button></td>
-        <td><button class="btn btn-outline-danger">Delete</button></td>
+        <td><button onclick=SetFormForUpdate(${i})  class="btn btn-outline-success">Update</button></td>
+        <td><button  onclick=DeleteProduct(${i}) class="btn btn-outline-danger">Delete</button></td>
     </tr>`
-
     };
+
     document.getElementById("tbody").innerHTML=Cartoona;
-
 }
-
-
 function SearchProduct(SearchTerm){
     var SearchResults=[]
     for(i=0;i<ProductsArray.length;i++){
@@ -64,3 +71,68 @@ function SearchProduct(SearchTerm){
     }
     displayProducts(SearchResults);
 }
+
+
+function DeleteProduct(DeletedProductIndex){
+    ProductsArray.splice(DeletedProductIndex,1);
+        localStorage.setItem("Products",JSON.stringify(ProductsArray));
+        displayProducts(ProductsArray);
+    }
+
+
+    function SetFormForUpdate(UpdateProductIndex){
+
+        ProductNameInput.value=ProductsArray[UpdateProductIndex].Name
+        ProductPriceInput.value=ProductsArray[UpdateProductIndex].Price
+        ProductCategoryInput.value=ProductsArray[UpdateProductIndex].Category
+        ProductDescriptionInput.value=ProductsArray[UpdateProductIndex].Description
+        UpdateButtton.classList.replace('d-none','d-inline-block')
+        Addbtn.classList.add('d-none')
+        UpdateButtton.onclick=()=>UpdateProduct(UpdateProductIndex);
+    }
+
+    function UpdateProduct(IndextobeUpdated) {
+        console.log("Updating product...");
+        console.log("IndextobeUpdated:", IndextobeUpdated);
+        console.log("ProductNameInput.value:", ProductNameInput.value);
+        console.log("ProductPriceInput.value:", ProductPriceInput.value);
+        console.log("ProductCategoryInput.value:", ProductCategoryInput.value);
+        console.log("ProductDescriptionInput.value:", ProductDescriptionInput.value);
+        // Update ProductsArray with new values
+        for (var i = 0; i < ProductsArray.length; i++) {
+            if (i == IndextobeUpdated) {
+                ProductsArray[i].Name = ProductNameInput.value;
+                ProductsArray[i].Price = ProductPriceInput.value;
+                ProductsArray[i].Category = ProductCategoryInput.value;
+                ProductsArray[i].Description = ProductDescriptionInput.value;
+                break;
+            }
+        }
+        console.log(ProductsArray)
+        
+        // Store the updated ProductsArray in local storage
+        localStorage.setItem("Products", JSON.stringify(ProductsArray));
+    }
+    
+    
+    
+function updated(){
+    displayProducts(ProductsArray);
+    
+}
+
+
+//validation
+function validation(){
+
+    
+    var regex=/^[A-Z][a-z]{3,8}$/;
+    if(regex.test(ProductNameInput.value)==true ){
+        ProductNameInput.classList.replace('is-invalid','is-valid')
+        return true
+    }
+    else{
+        
+        ProductNameInput.classList.add('is-invalid')
+        return false
+}}
